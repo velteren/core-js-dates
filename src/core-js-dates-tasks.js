e9173abf9d724f033cc73bdbf906c65950901f42
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2025-01-04 01:39:53 +0300

    feat: add getCountWeekendsInMonth

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -176,8 +176,17 @@ function formatDate(date) {
  * 12, 2023 => 10
  * 1, 2024 => 8
  */
-function getCountWeekendsInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountWeekendsInMonth(month, year) {
+  const date = new Date(year, month - 1);
+  let counter = 0;
+  do {
+    const day = date.getDay();
+    if (day === 0 || day === 6) {
+      counter += 1;
+    }
+    date.setDate(date.getDate() + 1);
+  } while (date.getDate() !== 1);
+  return counter;
 }
 
 /**
