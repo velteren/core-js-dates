acde37e939e3fe9b3ad9b176882a56f8aeb8ee48
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2025-01-03 23:49:42 +0300

    feat: add getNextFriday

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -73,8 +73,16 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const incValues = [5, 4, 3, 2, 1, 7, 6];
+  return new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate() + incValues[date.getDay()],
+    date.getHours(),
+    date.getMinutes(),
+    date.getSeconds()
+  );
 }
 
 /**
