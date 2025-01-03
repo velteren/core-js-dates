26bcfaf7ee6b4b2e22aa8103d7967725ec421b54
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2025-01-04 01:17:56 +0300

    feat: add formatDate

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -151,8 +151,17 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const d = new Date(date);
+  const timeStr = new Intl.DateTimeFormat('en-US', {
+    dateStyle: undefined,
+    timeStyle: 'medium',
+    timeZone: 'Atlantic/Reykjavik',
+  }).format(d);
+  const dateStr = new Intl.DateTimeFormat('en-US', {
+    timeZone: 'Atlantic/Reykjavik',
+  }).format(d);
+  return `${dateStr}, ${timeStr}`;
 }
 
 /**
