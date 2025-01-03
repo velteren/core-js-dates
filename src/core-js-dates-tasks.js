c07b39db36b98b26d664339a8057d25433b3b867
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2025-01-03 23:16:27 +0300

    feat: add getDayName

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -49,8 +49,17 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const DAY_NAMES = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  return DAY_NAMES[new Date(date).getDay()];
 }
 
 /**
